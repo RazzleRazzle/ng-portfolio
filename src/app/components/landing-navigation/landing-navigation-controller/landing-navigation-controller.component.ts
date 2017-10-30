6e7791c25e909d613d1d9547296268f7a122ba82
Jake A Musich <makejusich@gmail.com>
2017-10-30 00:20:05 -0400

    worked on nav controller

--- src/app/components/landing-navigation/landing-navigation-controller/landing-navigation-controller.component.ts
+++ src/app/components/landing-navigation/landing-navigation-controller/landing-navigation-controller.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing-navigation-controller.component.scss']
 })
 export class LandingNavigationControllerComponent implements OnInit {
+	private 
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  private navigate = (direction: string) => {
+  	console.log(direction)
   }
 
 }
